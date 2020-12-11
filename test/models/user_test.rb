addf4ddee1789be40211fb10411cdc8a5b6e55e6
Jacob Jackson <jacobjackson1985@gmail.com>
2020-12-10 19:34:28 -0600

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email validation should accpet valid addreses" do
+  test "email validation should accept valid addreses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
@@ -74,7 +74,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
 end
